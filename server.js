0d0f338aae7a40b489a41185f3939325fbfc852e
ibnballo <ibnballo@gmail.com>
2023-06-20 06:34:28 +0100

    Add log events

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const logEvents = require('./logEvent');
 const EventEmitter = require('events');
 
 const myEventEmitter = new EventEmitter();
-myEventEmitter.on('log', (msg) => {
+
+myEventEmitter.on('logs', (msg) => {
     console.log(logEvents(msg));
 });
 
 setTimeout(() => {
-    myEventEmitter.emit('log', 'From Emit');
-}, 2000)
+    myEventEmitter.emit('logs', 'Hi, from Emitter')
+});
