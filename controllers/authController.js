0cc5cf109ce9efd1170d030998dd86950100e5b0
ibnballo <ibnballo@gmail.com>
2023-06-23 15:12:33 +0100

    Add jsonwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,11 @@ const userDB = {
 }
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ "Message": "Username and Password are required" });
@@ -12,7 +17,27 @@ const handleLogin = async (req, res) => {
     // evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
-        res.json({'Success': `User ${user} is logged in`});
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s'}
+        );
+
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d'}
+        );
+
+        const otherUsers = userDB.users.filter(person => person.username !== foundUser);
+        const currentUser = { ...foundUser, refreshToken };
+        userDB.setUsers([...otherUsers, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(userDB.users)
+        );
+        res.cookie('jwt', refreshToken, {httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000});
+        res.json({accessToken});
     } else {
         res.sendStatus(401); // unauthorized
     }
--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const cors = require('cors');
 const { logger } = require('./middleware/logEvent');
 const errHandler = require('./middleware/errHandler');
 const corsOption = require('./config/corsOption');
-
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 // A customer middleware needs a next argument
 app.use(logger)
+
+app.use(credentials);
 
 // cors
 app.use(cors(corsOption));
@@ -20,11 +24,16 @@ app.use(express.urlencoded({extended: false})); // form form
 
 app.use(express.json()); // for json files
 
+app.use(cookieParser());
+
 app.use('/', express.static(path.join(__dirname, '/public'))); // For all static file
 
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
